b17952d4c25e5dfa355d838cf135af1acf611b5c
WYY0103 <3177620483@qq.com>
2022-09-06 16:32:16 +0800

    feat：计算属性

---
+++ study/vue3/vue-project/src/views/computed.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>{{ fullname }}</div>
+</template>
+
+<script setup>
+import { reactive } from "@vue/reactivity";
+import { computed } from "@vue/runtime-core";
+
+let name = reactive({
+  fullname: "li",
+  firstname: "si",
+});
+
+let fullname = computed(() => {
+  return name.fullname + name.firstname;
+});
+</script>
+
+<style>
+</style>
+
