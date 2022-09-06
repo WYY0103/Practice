14967c1d077380e15c243c33e96165400242c889
WYY0103 <3177620483@qq.com>
2022-09-06 16:33:07 +0800

    feat：生命周期

---
+++ study/vue3/vue-project/src/views/AboutView.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="about">
+    <!-- <h1>This is an about page</h1> -->
+    <p v-if="loading">loading.....</p>
+    <p v-else>{{data}}</p>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
+
+<script setup>
+import { onBeforeMount, onMounted } from "@vue/runtime-core";
+import {ref} from "vue";
+
+let data = ref('');
+let loading = ref(true);
+onBeforeMount(() => {
+  console.log(1111);
+});
+
+onMounted(() => {
+  console.log(2222);
+  setTimeout(()=>{
+    data.value = 'hhhhh';
+    loading.value = false;
+  },1000)
+});
+</script>
