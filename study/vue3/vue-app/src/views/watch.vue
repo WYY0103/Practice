19d672054013f298ef7cf53afa59b74db3bcd542
WYY0103 <3177620483@qq.com>
2022-09-07 09:09:17 +0800

    feat：监听

---
+++ study/vue3/vue-app/src/views/watch.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>name:{{ fullname }}</h1>
+  </div>
+</template>
+
+<script setup>
+import { reactive, ref, watch } from "vue";
+
+const name = reactive({
+  lastname: "li",
+  firstname: "si",
+});
+
+const fullname = ref("li si");
+
+watch(name, (newValue, oldValue) => {
+  fullname.value = `${newValue.lastname} ${newValue.firstname}`;
+});
+</script>
+
